56648d70e938457e0ad23ce81572625adc830a78
sarahbarron <sarahflavin@yahoo.com>
2020-07-12 21:43:54 +0100

    fixed svg link to the header logo from lower case link to capital Link

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./header.styles.scss";
-// Reacts way of importing SVG
+// Reacts way of importing SVG. tells react you want the render the SVG as a component
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { Link } from "react-router-dom";
 
 const Header = () => (
   <div className="header">
-    <link className="logo-container" to="/">
-      {/* <Logo className='logo' /> */}
-    </link>
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
 
     <div className="options">
       <Link className="option" to="/shop">
